e9e6a1df8614cebd45b3a5d7dbdd03a501fd20b9
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-25 21:33:35 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -2,6 +2,7 @@ import json
 import ipdb
 import glob
 import os
+import re
 
 name_json_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/name_matchedList.json"
 name_dicts = json.load(open(name_json_path))
@@ -10,10 +11,22 @@ wav_dir = "/home/gyzhang/speech_database/CSTelling/20200913_120655_clean_ph_lab/
 generated_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/wavs/"
 
 ### process generate wavs #######
+
+generated_names_dict = {}
 for wav_path in glob.glob(f'{generated_path}/*.wav'):
     item_name = os.path.basename(wav_path)
+    res_list = re.findall(r'\[(.*?)\]', item_name)
+    assert len(res_list) == 2
+    name_clip_name = res_list[-1]
+    seg_name = name_clip_name[:-4]
+    clips = re.split(r"\-", seg_name)
+    name_clip_in_seg_index = clips[-2]
+    name_index = clips[-1]
     ipdb.set_trace()
+    generated_names_dict[name_clip_name] = [
+        seg_name, name_clip_in_seg_index, name_index]
 
 
 for wav_path in glob.glob(f'{wav_dir}/*.wav'):
-    item_name = os.path.basename(wav_path)
+    item_seg_name = os.path.basename(wav_path)
+    ipdb.set_trace()
