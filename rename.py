54133405b4a4c29e6eeabdd12cd972da0042b4cd
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-25 21:17:45 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -10,7 +10,7 @@ wav_dir = "/home/gyzhang/speech_database/CSTelling/20200913_120655_clean_ph_lab/
 generated_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/wavs/"
 
 ### process generate wavs #######
-for wav_path in glob.glob(f'{wav_dir}/*.wav'):
+for wav_path in glob.glob(f'{generated_path}/*.wav'):
     item_name = os.path.basename(wav_path)
     ipdb.set_trace()
 
