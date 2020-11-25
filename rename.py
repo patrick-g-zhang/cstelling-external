aef5fcbf30f592842a9013259a6f5a3c84ed33b5
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-25 21:16:34 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,8 +1,19 @@
 import json
 import ipdb
+import glob
+import os
 
-ipdb.set_trace()
 name_json_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/name_matchedList.json"
 name_dicts = json.load(open(name_json_path))
 
-pass
+wav_dir = "/home/gyzhang/speech_database/CSTelling/20200913_120655_clean_ph_lab/wavs"
+generated_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/wavs/"
+
+### process generate wavs #######
+for wav_path in glob.glob(f'{wav_dir}/*.wav'):
+    item_name = os.path.basename(wav_path)
+    ipdb.set_trace()
+
+
+for wav_path in glob.glob(f'{wav_dir}/*.wav'):
+    item_name = os.path.basename(wav_path)
