8742938883295e9787b5754e6ffd8a1060fe1dcf
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-25 20:20:06 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,1 +1,5 @@
 import json
+import pdb
+
+name_json_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/name_matchedList.json"
+name_dicts = json.load(name_json_path)
