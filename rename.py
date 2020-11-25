30dbe523bb147615122929326c03e4b55a6678a4
ZHANG GUANGYAN <guangyanee@gmail.com>
2020-11-25 20:28:11 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -3,4 +3,6 @@ import ipdb
 
 ipdb.set_trace()
 name_json_path = "/home/gyzhang/fastspeech2-master/checkpoints/fs2_ref_utt_story/generated_200000_/name_matchedList.json"
-name_dicts = json.load(name_json_path)
+name_dicts = json.load(open(name_json_path))
+
+pass
